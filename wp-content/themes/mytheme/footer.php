88a2c7618a01f9240cbb9ebbf0060d2e019ed5f0
Yoshihide Chubachi <yoshi@chubachi.net>
2014-02-03 01:26:36 +0900

    Update footer.php

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -2,7 +2,7 @@
 <p>COPYRIGHT &copy; ADVANCED INSTITUTE OF INDUSTRIAL TECHNOLOGY ALL RIGHTS RESERVED.<br>
 このウェブサイト上の文章、映像、写真などの著作物の全部、または一部を了承なく複製、使用することを禁じます。<br>
 <br>
-AIIT Showcaseは産業技術学院大学が運営しております。<br>
+AIIT Showcaseは産業技術大学院大学が運営しております。<br>
 本サイトの内容と<a href="http://aiit.ac.jp/" target="_blank">本学ホームページ</a>の内容とに相違がある場合はホームページの内容を優先します。<br>
 本サイトの記事に関するご質問・ご意見などは本学事務局まで直接ご連絡ください。</p>
 </footer>
