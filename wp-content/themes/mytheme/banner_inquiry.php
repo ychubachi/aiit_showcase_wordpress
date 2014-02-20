43817cd91faaa7f2fc81e9eddfbb3a1beb83c317
Yoshihide Chubachi <yc@aiit.ac.jp>
2014-02-20 15:20:29 +0900

    2/22,24説明会情報に更新

--- wp-content/themes/mytheme/banner_inquiry.php
+++ wp-content/themes/mytheme/banner_inquiry.php
@@ -1,13 +1,14 @@
 <!-- banner_inquiry.php -->
 
 <section>
-<p class="mt25"><a href="https://aiit.ac.jp/form/document/add" target="_blank"><img src="/images/common/common_bn01.jpg" width="615" height="100" alt="産業技術大学院大学についてのお問い合わせ・資料請求はこちら"></a></p>
+<div align="center">
+<a class="doorkeeper-registration-widget" data-width="300" href="http://aiit.doorkeeper.jp/events/9225" target="_blank">産業技術大学院大学（AIIT）入試説明会（2/22開催）</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
+<a class="doorkeeper-registration-widget" data-width="300" href="http://aiit.doorkeeper.jp/events/9226" target="_blank">産業技術大学院大学（AIIT）入試説明会（2/24開催）</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
+</div>
 </section>
 
 <section>
-<div align="center">
-<a class="doorkeeper-registration-widget" data-width="615" href="http://aiit.doorkeeper.jp/events/8988">産業技術大学院大学（AIIT）入試説明会（2/19開催）</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
-</div
+<p class="mt25"><a href="https://aiit.ac.jp/form/document/add" target="_blank"><img src="/images/common/common_bn01.jpg" width="615" height="100" alt="産業技術大学院大学についてのお問い合わせ・資料請求はこちら"></a></p>
 </section>
 
 <!-- /banner_inquiry.php -->
