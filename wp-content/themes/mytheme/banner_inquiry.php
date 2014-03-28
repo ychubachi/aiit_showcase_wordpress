88dbe01e536f8846c98a557527a356f599601455
Yoshihide Chubachi <yc@aiit.ac.jp>
2014-03-28 15:55:48 +0900

    説明会の情報をコメントアウトしました

--- wp-content/themes/mytheme/banner_inquiry.php
+++ wp-content/themes/mytheme/banner_inquiry.php
@@ -1,11 +1,15 @@
 <!-- banner_inquiry.php -->
 
+<?php
+ /*
 <section>
 <div align="center">
 <a class="doorkeeper-registration-widget" data-width="300" href="http://aiit.doorkeeper.jp/events/9544" target="_blank">産業技術大学院大学（AIIT）入試説明会（3/5開催）</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
 <a class="doorkeeper-registration-widget" data-width="300" href="http://aiit.doorkeeper.jp/events/9545" target="_blank">産業技術大学院大学（AIIT）入試説明会（3/12開催）</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
 </div>
 </section>
+ */
+?>
 
 <section>
 <p class="mt25"><a href="https://aiit.ac.jp/form/document/add" target="_blank"><img src="/images/common/common_bn01.jpg" width="615" height="100" alt="産業技術大学院大学についてのお問い合わせ・資料請求はこちら"></a></p>
