54bc27bf247fd5b218b6492ece612558ae84b8a5
Yoshihide Chubachi <yc@aiit.ac.jp>
2014-11-18 15:13:39 +0900

    Update

--- wp-content/themes/mytheme/banner_inquiry.php
+++ wp-content/themes/mytheme/banner_inquiry.php
@@ -1,10 +1,12 @@
 <!-- banner_inquiry.php -->
 
+<!--
 <section>
 <div align="center">
 <a class="doorkeeper-registration-widget" data-width="615" href="http://aiit.doorkeeper.jp/events/12692">3Dプリンターが起こす新しい設計と開発体制の変化</a><script src="http://widgets.doorkeeper.jp/w/widget.js"></script>
 </div>
 </section>
+-->
 
 <section>
 <p class="mt25"><a href="https://aiit.ac.jp/form/document/add" target="_blank"><img src="/images/common/common_bn01.jpg" width="615" height="100" alt="産業技術大学院大学についてのお問い合わせ・資料請求はこちら"></a></p>
